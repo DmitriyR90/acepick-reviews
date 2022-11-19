400972a74395f818ed11813c5ebbdf5ee297c460
DmitriyR90 <ryabokon.dn@gmail.com>
2022-11-19 12:54:09 +0200

    Initial commit

---
+++ js/base-script.js
@@ -0,0 +1,38 @@
+(() => {
+  // open modal window
+  const refs = {
+    openModalBtn: document.querySelector('.header__burger'),
+    modal: document.querySelector('.header__menu'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('active');
+    refs.openModalBtn.classList.toggle('active');
+    document.body.classList.toggle('lock');
+  }
+
+  //banking accordion menu
+  document.querySelectorAll('.nav-item__btn').forEach((el) => {
+    el.addEventListener('click', () => {
+      let content = el.nextElementSibling;
+
+      if (content.style.maxHeight) {
+        document
+          .querySelectorAll('.nav-item__list')
+          .forEach((el) => (el.style.maxHeight = null));
+        el.classList.remove('opened-list');
+      } else {
+        document
+          .querySelectorAll('.nav-item__list')
+          .forEach((el) => (el.style.maxHeight = null));
+
+        document.querySelectorAll('.nav-item__btn').forEach((btn) => {
+          btn.classList.remove('opened-list');
+        });
+
+        content.style.maxHeight = content.scrollHeight + 'px';
+        el.classList.add('opened-list');
+      }
+    });
+  });
+})();
